8c427f6299355484c5feb5af4abcd36a8b715c00
David Vilca <davidvilcao@gmail.com>
2018-01-30 02:29:35 -0500

    JS Velocity Scroll

--- js/menu.js
+++ js/menu.js
@@ -9,8 +9,23 @@ headroom.init();
 
 
 
-var scroll = new SmoothScroll('a[href*="#about"]');
 
+
+var scroll = new SmoothScroll('a[href*="#about"]', {
+	// Selectors
+	ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+	header: null, // Selector for fixed headers (must be a valid CSS selector)
+
+	// Speed & Easing
+	speed: 800, // Integer. How fast to complete the scroll in milliseconds
+	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+	easing: 'easeInOutCubic', // Easing pattern to use
+	customEasing: function (time) {}, // Function. Custom easing pattern
+
+	// Callback API
+	before: function () {}, // Callback to run before scroll
+	after: function () {} // Callback to run after scroll
+});
 });
 
 
